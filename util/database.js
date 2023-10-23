11f66bae217c2076b13c13b4475b59b7f057a4a6
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-10-23 12:07:47 +0530

    Task 1: getting started with mongoDb

---
+++ util/database.js
@@ -0,0 +1,29 @@
+const mongoDB = require("mongodb");
+const MongoClient = mongoDB.MongoClient;
+
+let _db;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+
+    "mongodb+srv://admin:admin@sharpenernosql.nsxzmxe.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getDB = () => {
+  if (_db) {
+    return _db;
+  } else {
+    throw "No database found";
+  }
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
