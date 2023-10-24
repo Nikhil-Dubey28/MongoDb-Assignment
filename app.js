06b69399025217d8b637d7327f96ebf4af5cbb35
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-10-24 14:24:06 +0530

    Task 9: user table

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,12 +18,12 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("6536439c55f3a283d90da185").then((user) => {
-//     req.user = new User(user.name, user.email, user.cart, user._id);
-//     next();
-//   });
-// });
+app.use((req, res, next) => {
+  User.findById("6536439c55f3a283d90da185").then((user) => {
+    req.user = user;
+    next();
+  });
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -32,10 +32,21 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    
-   "mongodb+srv://admin:admin@sharpenernosql.nsxzmxe.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://admin:admin@sharpenernosql.nsxzmxe.mongodb.net/?retryWrites=true&w=majority"
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "nidhi",
+          email: "nidhi@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch((err) => {
